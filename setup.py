1589048eb00d168fa1db414028fa7ac8d7709718
d-v-b <davis.v.bennett@gmail.com>
2019-06-11 11:30:37 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sheetscrape",
+    version="0.0.0",
+    author="Davis Bennett",
+    packages=find_packages(exclude=["*.pyc"]),
+    include_package_data=True,
+    install_requires=["gspread", "Pandas"]
+    )
